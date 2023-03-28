12ae2d20688173506eccdd101811506ebb4cfbcc
2021108102540049 <2021108102540049@lab01-07.ifgoiano.local>
2023-03-28 10:41:36 -0300

    Adicionando bootstrap no formulario

--- cadastro-web/src/main/java/br/edu/ifgoiano/entidade/CadastroUsuarioServlet.java
+++ cadastro-web/src/main/java/br/edu/ifgoiano/entidade/CadastroUsuarioServlet.java
@@ -1,6 +1,8 @@
 package br.edu.ifgoiano.entidade;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -11,8 +13,20 @@ import javax.servlet.http.HttpServletResponse;
 @WebServlet("/cadastrarUsuario")
 public class CadastroUsuarioServlet extends HttpServlet {
 
+	//simulando banco de dados 
+	
+	private List<Usuario>lstDeUsuario;
+	
+	@Override
+	public void init() throws ServletException {
+		this.lstDeUsuario= new ArrayList<Usuario>();	}
+	
+	
+	
 	@Override
 	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		//
+		
 		//
 		String senha1= req.getParameter("senha");
 		String senha2= req.getParameter("senha2");
@@ -21,6 +35,13 @@ public class CadastroUsuarioServlet extends HttpServlet {
 		
 		//verifica se a senhas estão iguais 
 		if(senha1.equals(senha2)) {
+			Usuario usu = new Usuario();
+			usu.setNome(req.getParameter("name"));
+			usu.setEmail(req.getParameter("email"));
+			usu.setSenha(senha1);
+			
+			lstDeUsuario.add(usu);
+			
 			//redireciona o usuario para o login
 			resp.sendRedirect("index.html");
 			
@@ -34,7 +55,26 @@ public class CadastroUsuarioServlet extends HttpServlet {
 		}
 		
 		
+		
 	}
 	
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		for (Usuario usuario : lstDeUsuario) {
+			System.out.println(usuario.getNome().concat("-").concat(usuario.getEmail()));
+			
+			
+		}
+	}
+	
+	
+	//apaga os elementos da lista 
+	@Override
+	public void destroy() {
+		this.lstDeUsuario.clear();
+	}
+	
+	
+
 	
 }
