e2bfe64de0ae3c99d18ecb44ce9877500432b315
2021108102540049 <2021108102540049@lab01-07.ifgoiano.local>
2023-06-20 10:45:13 -0300

    adicionando banco de dados

--- cadastro-web/src/main/java/br/edu/ifgoiano/repositorio/UsuarioRepositorio.java
+++ cadastro-web/src/main/java/br/edu/ifgoiano/repositorio/UsuarioRepositorio.java
@@ -1,5 +1,71 @@
 package br.edu.ifgoiano.repositorio;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+import br.edu.ifgoiano.entidade.Usuario;
+
 public class UsuarioRepositorio {
 
+	
+	public static Connection conn;
+	
+	public UsuarioRepositorio() {
+		try {
+			Connection conn = DriverManager.
+					getConnection("jdbc:h2:~/usuariodb", "sa", "sa");
+			
+			System.out.println("Conexão realizada com sucesso.");
+		} catch (SQLException e) {
+			System.out.println("Erro na conexão com o banco de dados.");
+			e.printStackTrace();
+		}
+		
+		
+			
+	}
+
+	public List<Usuario> ListarUsuarios(){
+		List<Usuario> lstusuarios = new  ArrayList<Usuario>();
+		
+		String sql= "select id,nome, email,senha,data_nascimento from usuario";
+		
+		try {
+			PreparedStatement  pst =conn.prepareStatement(sql);
+			
+			ResultSet resultSet = pst.executeQuery();
+			
+			while (resultSet.next()) {
+				
+				Usuario usuario = new Usuario();
+				usuario.setId(resultSet.getInt("id"));
+				usuario.setNome(resultSet.getString("nome"));
+				usuario.setEmail(resultSet.getString("email"));
+				usuario.setDataNascimento(resultSet.getDate("data_nascimento"));
+				
+				
+				lstusuarios.add(usuario);
+			}
+			
+		
+				
+		
+		}
+		catch(Exception e) {
+			System.out.println("Erro na consulta de usuario ");
+			e.printStackTrace();
+		}
+	return lstusuarios	;
+	}
+	
+	
+	
+	
+	
+	
 }
